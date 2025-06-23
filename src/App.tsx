29ef8983d2bc8e2e55b1783fcd2d317e67c72928
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-23 11:34:41 -0300

    conteudo e rodape

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
+import Rodape from "./Rodape";
 
 export default function App() {
   // Comentário de uma linha
@@ -6,46 +8,8 @@ export default function App() {
   return (
     <>
       <Cabecalho />
-      <main>
-        <section>
-          <h2> Seja bem-vindo(a) aos Fundamentos de React!</h2>
-          <p> Este é um exemplo de aplicação React</p>
-
-          <article>
-            <h3>Artigo 1 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-              consequatur dolore accusamus distinctio aut dolorem totam earum
-              minus dicta dignissimos obcaecati est id eligendi quasi, dolores
-              itaque fugit qui? Quam?
-            </p>
-          </article>
-          <article>
-            <h3>Artigo 2 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-              consequatur dolore accusamus distinctio aut dolorem totam earum
-              minus dicta dignissimos obcaecati est id eligendi quasi, dolores
-              itaque fugit qui? Quam?
-            </p>
-          </article>
-          <article>
-            <h3>Artigo 3 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-              consequatur dolore accusamus distinctio aut dolorem totam earum
-              minus dicta dignissimos obcaecati est id eligendi quasi, dolores
-              itaque fugit qui? Quam?
-            </p>
-          </article>
-        </section>
-      </main>
-      <footer>
-        <h2>Pequeno site criado com React</h2>
-        <p>
-          Desenvolvido por <b> Heloisa</b> &copy; 2025
-        </p>
-      </footer>
+      <Conteudo />
+      <Rodape />
     </>
   );
 }
--- src/Conteudo.tsx
+++ src/Conteudo.tsx
@@ -1,1 +1,38 @@
-export default function Conteudo() {}
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2> Seja bem-vindo(a) aos Fundamentos de React!</h2>
+        <p> Este é um exemplo de aplicação React</p>
+
+        <article>
+          <h3>Artigo 1 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+            consequatur dolore accusamus distinctio aut dolorem totam earum
+            minus dicta dignissimos obcaecati est id eligendi quasi, dolores
+            itaque fugit qui? Quam?
+          </p>
+        </article>
+        <article>
+          <h3>Artigo 2 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+            consequatur dolore accusamus distinctio aut dolorem totam earum
+            minus dicta dignissimos obcaecati est id eligendi quasi, dolores
+            itaque fugit qui? Quam?
+          </p>
+        </article>
+        <article>
+          <h3>Artigo 3 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+            consequatur dolore accusamus distinctio aut dolorem totam earum
+            minus dicta dignissimos obcaecati est id eligendi quasi, dolores
+            itaque fugit qui? Quam?
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+}
