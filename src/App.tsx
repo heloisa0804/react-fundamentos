29da6736ba8aa1fc6dbd930671e36ea4d6fdd625
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-23 10:02:51 -0300

    adiciona fragmentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 export default function App() {
   // Comentário de uma linha
   /* Comentário de múltiplas linhas */
+  // <> </> -> Fragmento (React Fragmento)
   return (
-    <header>
-      <h1>Olá React! 😁😁</h1>
-      <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Sobre</a>
-        <a href="">Contato</a>
-      </nav>
-    </header>
+    <>
+      <header>
+        <h1>Olá React! 😁😁</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Sobre</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
+      <main></main>
+    </>
   );
 }
