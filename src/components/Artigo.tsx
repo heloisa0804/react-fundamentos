ebc5ef0bbbc68a4572cef5123a94e735a70ffbea
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-24 09:13:09 -0300

    Adiciona componente Artigo

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,13 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de exemplo</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+        consequatur dolore accusamus distinctio aut dolorem totam earum minus
+        dicta dignissimos obcaecati est id eligendi quasi, dolores itaque fugit
+        qui? Quam?
+      </p>
+    </article>
+  );
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,33 +7,11 @@ export default function Conteudo() {
         <h2> Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p> Este é um exemplo de aplicação React</p>
 
-        <article>
-          <h3>Artigo 1 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-            consequatur dolore accusamus distinctio aut dolorem totam earum
-            minus dicta dignissimos obcaecati est id eligendi quasi, dolores
-            itaque fugit qui? Quam?
-          </p>
-        </article>
-        <article>
-          <h3>Artigo 2 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-            consequatur dolore accusamus distinctio aut dolorem totam earum
-            minus dicta dignissimos obcaecati est id eligendi quasi, dolores
-            itaque fugit qui? Quam?
-          </p>
-        </article>
-        <article>
-          <h3>Artigo 3 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-            consequatur dolore accusamus distinctio aut dolorem totam earum
-            minus dicta dignissimos obcaecati est id eligendi quasi, dolores
-            itaque fugit qui? Quam?
-          </p>
-        </article>
+        <Artigo />
+
+        <Artigo />
+
+        <Artigo />
       </section>
     </main>
   );
