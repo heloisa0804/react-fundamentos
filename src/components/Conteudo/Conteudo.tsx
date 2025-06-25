b3e1ba9297a8db719989265df4b321f56f08d3f3
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-25 10:41:57 -0300

    Adiciona regras Styled Components

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -6,7 +6,9 @@ export default function Conteudo() {
   return (
     <main className={estilos.conteudo}>
       {/* Para combinar classes, use templete string */}
-      <section className={`${estilos.arredondada} ${estilos.sombraS}`}>
+      <section
+        className={`${estilos.principal}${estilos.arredondada} ${estilos.sombraS}`}
+      >
         <h2> Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p> Este é um exemplo de aplicação React</p>
 
--- src/components/DicaDoDia/DicaDoDia.tsx
+++ src/components/DicaDoDia/DicaDoDia.tsx
@@ -1,8 +1,38 @@
 import styled from "styled-components";
 
 const StyledDicaDoDia = styled.section`
-  background-color: #e8f5e9;
+  background-color: #10ca1f;
   border-left: 5px solid darkgreen;
+  padding: 1rem;
+  margin-top: 1rem;
+  border-radius: 4px;
+
+  h3 {
+    margin-top: 0;
+    color: darkblue;
+  }
+
+  p {
+    color: darkblue;
+    font-size: 1.1rem;
+  }
+
+  .site-oficial {
+    background-color: white;
+    text-align: center;
+    padding: 0.4rem;
+
+    a {
+      text-decoration: none;
+      color: darkcyan;
+      font-weight: bold;
+
+      &:hover,
+      &:focus {
+        text-decoration: underline;
+      }
+    }
+  }
 `;
 
 export default function DicaDoDia() {
@@ -14,7 +44,7 @@ export default function DicaDoDia() {
         Neste exemplo, estamos usando <b>styled-componets</b>, uma famosa
         biblioteca para CSS In JS
       </p>
-      <p>
+      <p className="site-oficial">
         Site oficial:{" "}
         <a href="https://styled-components.com/" target="_blank">
           Styled Components
