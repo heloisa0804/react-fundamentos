5b5139d80bba3373cd522ceb88a90e35cd8781bf
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-26 09:57:18 -0300

    Adiciona data de registro  nos artigos

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -16,7 +16,7 @@ export default function Conteudo() {
         <Saudacao classe="bg-red-100" />
         <p> Este é um exemplo de aplicação React</p>
 
-        <Artigo titulo="Front-End" numero={1}>
+        <Artigo titulo="Front-End" numero={1} data={new Date()}>
           {" "}
           <p>
             Front-End é muito bacana, a gente usa HTML com CSS e Java Script
