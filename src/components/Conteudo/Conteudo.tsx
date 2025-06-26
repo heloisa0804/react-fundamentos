408619189efc0992aa95707791aa3b040fe23994
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-26 08:57:56 -0300

    Usa mais props e template string

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -11,7 +11,9 @@ export default function Conteudo() {
       <section
         className={`${estilos.principal}${estilos.arredondada} ${estilos.sombraS}`}
       >
-        <Saudacao nome="Heloisa" />
+        <h2 className="font-bold text-lg">Fundamentos de React!</h2>
+        <Saudacao nome="Heloisa" classe="bg-emerald-400" />
+        <Saudacao nome="Camila" classe="bg-red-100" />
         <p> Este é um exemplo de aplicação React</p>
 
         <Artigo />
--- src/components/Conteudo/Saudacao.tsx
+++ src/components/Conteudo/Saudacao.tsx
@@ -1,15 +1,14 @@
 //Declara um tipo que sera usado pelas props do Componente
 type Props = {
   nome: string;
+  classe: string;
 };
 
 export default function Saudacao(props: Props) {
   return (
     <>
-      <h2 className="font-bold text-lg">Fundamentos de React!</h2>
-
-      <p className="my-2">
-        Bem-vindo(a) <b>{props.nome}</b>
+      <p className={`my-2 rounded text-center ${props.classe}`}>
+        Bem-vindo(a) <b>{props.nome} </b>
       </p>
     </>
   );
