7742f1919f5196500908aa2fb6231620e3b81965
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-26 09:04:17 -0300

    usa desestruturação

--- src/components/Conteudo/Saudacao.tsx
+++ src/components/Conteudo/Saudacao.tsx
@@ -1,14 +1,14 @@
 //Declara um tipo que sera usado pelas props do Componente
-type Props = {
+type SaudacaoProps = {
   nome: string;
   classe: string;
 };
 
-export default function Saudacao(props: Props) {
+export default function Saudacao({ nome, classe }: SaudacaoProps) {
   return (
     <>
-      <p className={`my-2 rounded text-center ${props.classe}`}>
-        Bem-vindo(a) <b>{props.nome} </b>
+      <p className={`my-2 rounded text-center ${classe}`}>
+        Bem-vindo(a) <b>{nome}</b>
       </p>
     </>
   );
