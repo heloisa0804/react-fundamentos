ba1f6a91e068e46436f2c12b7da3c67497a1e812
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-26 09:43:54 -0300

    Adiciona texto diferentes em arquivos artigos

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -16,19 +16,33 @@ export default function Conteudo() {
         <Saudacao classe="bg-red-100" />
         <p> Este é um exemplo de aplicação React</p>
 
-        <Artigo titulo="Front-End" texto="Um texto qualquer" numero={1} />
+        <Artigo titulo="Front-End" numero={1}>
+          {" "}
+          <p>
+            Front-End é muito bacana, a gente usa HTML com CSS e Java Script
+          </p>
+          <p> Mas também dá dor de cabeça quando usado</p>
+        </Artigo>
 
-        <Artigo
-          titulo="Back-End"
-          texto="Um texto qualquer sobre Back"
-          numero={2}
-        />
+        <Artigo titulo="Back-End" numero={2}>
+          <p>Importante para estudar:</p>
+          <ul className="*:list-dic *:ml-m7">
+            <li>PHP</li>
+            <li>SQL</li>
+            <li>Orientação e objeto</li>
+          </ul>
+          <p>E muito mais</p>
+        </Artigo>
 
-        <Artigo
-          titulo="Mobile"
-          texto="Um texto qualquer sobre Mobile"
-          numero={3}
-        />
+        <Artigo titulo="Mobile" numero={3}>
+          <details>
+            <summary>Conheça as tecnologias maus usadas</summary>
+            <ol className="*:list-decimal *:ml-7">
+              <li>JavaScript</li>
+              <li>Libs e Frameworks</li>
+            </ol>
+          </details>
+        </Artigo>
       </section>
 
       <DicaDoDia />
