9deafe84acbb532d88b15894d3e6e019647f8981
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-06-27 11:55:56 -0300

    adiciona quantidade e seleciona quanteidade de cursos por categoria

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -46,7 +46,7 @@ export default function Conteudo() {
             </p>
           </>
         )}
-        <ListaCursos />
+        <ListaCursos categoria={categoriAtiva} />
       </section>
 
       <DicaDoDia />
